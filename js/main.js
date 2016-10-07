19121c185f97ca26667928e27e68b6936dbf5a29
Andrey.Voropay <andreyv@marka-software.com>
2016-10-07 19:22:20 +0300

    Freq

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 /**
  * Created by Andrey.Voropay on 10/7/2016.
  */
+var TestString = "Алгоритм Хаффмана — жадный алгоритм оптимального префиксного кодирования алфавита с минимальной избыточностью.",
+	TestString1 = "В чащах юга жил был цитрус - да, но фальшивый экземпляр!";
+
+function Freq(InputString) {
+	var i, treeLeaves = {};
+	for (i = 0; i < InputString.length; i++) {
+		if (InputString[i] in treeLeaves) {
+			treeLeaves[InputString[i]] = treeLeaves[InputString[i]] + 1;
+		} else {
+			treeLeaves[InputString[i]] = 1;
+		}
+	}
+	return treeLeaves;
+}
